cf1b7e3d0ae542576921922f024f9390f4bd5326
Sajeewa Dayaratne <sajmail@gmail.com>
2015-11-08 21:26:23 +1300

    Cleanup the configuration file for Jira REST
    Signed-off-by: Sajeewa Dayaratne <sajmail@gmail.com>

--- cfg/jirarest.cfg.php
+++ cfg/jirarest.cfg.php
@@ -7,24 +7,26 @@
 
 //-----------------------------------------------------------------------------------------
 /* The following parameters are not in use. */
-define('BUG_TRACK_DB_TYPE', '[Not in Use]');
-define('BUG_TRACK_DB_NAME', '[Not in Use]');
+define('BUG_TRACK_DB_TYPE',    '[Not in Use]');
+define('BUG_TRACK_DB_NAME',    '[Not in Use]');
 define('BUG_TRACK_DB_CHARSET', '[Not in Use]');
-define('BUG_TRACK_DB_USER', '[Not in Use]');
-define('BUG_TRACK_DB_PASS', '[Not in Use]');
+define('BUG_TRACK_DB_USER',    '[Not in Use]');
+define('BUG_TRACK_DB_PASS',    '[Not in Use]');
 //-----------------------------------------------------------------------------------------
 
 /** The Username being used by JIRA logon */
-define('BUG_TRACK_USERNAME', 'testlink');
+define('BUG_TRACK_USERNAME', '<username>');
 
 /** The Password being used by JIRA logon*/
-define('BUG_TRACK_PASSWORD', 'testlinksqa');
-/** link of the web server for JIRA*/
+define('BUG_TRACK_PASSWORD', '<password>');
+
+/** link of the web server for JIRA. This is an example, replace before use */
 define('BUG_TRACK_HREF',"http://jira/rest/api/latest/");
 
-/** link of the web server for jira ticket*/
+/** Link to view an issue in Jira (the key will be appended to this link). This is an example, replace before use*/
 define('BUG_TRACK_SHOW_BUG_HREF', "http://jira/browse/");
-/** link of the web server for creating new jira ticket*/
+
+/** Link to Jira for creating a new issue. This is an example, replace before use*/
 define('BUG_TRACK_ENTER_BUG_HREF',"http://jira/secure/CreateIssue!default.jspa");
 
 ?>
